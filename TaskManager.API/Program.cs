03c51d89c3d7bd6abafa701d670b846c33f659c0
Reshma C R <crreshma90@gmail.com>
2026-07-19 09:39:47 +0530

    feat: add database migration on application startup

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -78,6 +78,12 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+} 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
